0dfcb3fa6f216c7425665c1db34173f297250d97
Alexander-Agu <firstsonagu@gmail.com>
2025-08-06 10:27:05 +0200

    Adding framer motion animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,33 +3,71 @@ import "./hero.css"
 import { IoLocationOutline } from "react-icons/io5";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { socialLinks } from './HeroTools';
+import { motion } from 'motion/react';
+
 
 export default function Hero() {
+  let time1 = 0.6;
+  let time2 = 0.7;
+  let time3 = 0.8;
+
+  const itemVariants = (time) => { return{
+    hidden: { opacity: 0, y: 75 },
+    visible: { opacity: 1, y: 0, transition: { duration: time, ease: "easeOut" } },
+  }
+};
+
+
   return (
     <article className='hero-container'>
-        <section class="hero-section">
-            <h2 className='hero-title'>Hi, my name is <span className='gradient-text'>Alexander Agu</span></h2>
-            <p className='hero-intro'>A passionate <span className='green-text'>full-stack developer</span> and <span className='green-text'>tech mentor</span>, focused on building impactful web applications and cloud-integrated systems.</p>
+        <motion.div
+        variants={{
+            hidden: { opacity: 0, y: 75 },
+            visible: { opacity: 1, y: 0 }
+        }}
+        initial="hidden"
+        animate="visible"
+        transition={{duration: 0.5, delay: 0.25}}
+        className="hero-section"
+        >
 
-            <div className='hero-location'>
-                <IoLocationOutline style={{"color": "hsl(142, 71%, 45%)", "fontWeight": "bolder", "fontSize": "1.5rem"}} />
+            <motion.h2 className="hero-title" variants={itemVariants(0.6)}>
+                Hi, my name is <span className="gradient-text">Alexander Agu</span>
+            </motion.h2>
+
+            <motion.p variants={itemVariants(0.8)} className="hero-intro">
+                A passionate <span className="green-text">full-stack developer</span> and
+                <span className="green-text"> tech mentor</span>, focused on building impactful web applications and cloud-integrated systems.
+            </motion.p>
+
+            <div className="hero-location">
+                <IoLocationOutline
+                style={{
+                    color: "hsl(142, 71%, 45%)",
+                    fontWeight: "bolder",
+                    fontSize: "1.5rem"
+                }}
+                />
                 <p>Based in South Africa, Gauteng, Rosebank</p>
             </div>
 
-            <nav className='hero-navbar'>
-                <a href="" className='hero-direct'>View my work</a>
-                <a href="" download={true} className='hero-download'><MdOutlineFileDownload /> <p>Download Resume</p></a>
+            <nav className="hero-navbar">
+                <a href="#" className="hero-direct">View my work</a>
+                <a href="#" download className="hero-download">
+                <MdOutlineFileDownload />
+                <p>Download Resume</p>
+                </a>
             </nav>
 
             <div className="hero-socials">
-                {
-                    socialLinks.map(x => {
-                        const { icon, link } = x;
-                        return <a href={link}> {icon} </a>
-                    })
-                }
+                {socialLinks.map(({ icon, link }, index) => (
+                <a href={link} key={index}>
+                    {icon}
+                </a>
+                ))}
             </div>
-        </section>
+
+    </motion.div>
   
 
         <section class="animated-background">
